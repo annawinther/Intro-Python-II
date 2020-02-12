3affbd96df8fd5677381729e703337aefb8ce9f6
Anna Winther <anski.anna@gmail.com>
2020-02-12 15:34:20 +0000

    Fixed issue with error. Now user can move into rooms correctly

--- src/room.py
+++ src/room.py
@@ -2,13 +2,13 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, n_to = 0, e_to = 0, w_to = 0, s_to = 0, ):
         self.name = name
         self.description = description
-        self.n_to = None
-        self.e_to = None
-        self.w_to = None
-        self.s_to = None
+        self.n_to = n_to
+        self.e_to = e_to
+        self.w_to = w_to
+        self.s_to = s_to
 
 
     def __str__(self):
