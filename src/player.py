dee54cd01e2b2f63261930a5b138cb7cee61e3c9
Anna Winther <anski.anna@gmail.com>
2020-02-13 12:00:57 +0000

    Change Player class to contain current room instead of room

--- src/player.py
+++ src/player.py
@@ -3,9 +3,9 @@
 
 class Player:
 
-    def __init__(self, name, room):
+    def __init__(self, name, current_room):
         self.name = name
-        self.room = room
+        self.current_room = current_room
 
     def __str__(self):
-        return f'  Welcome, {self.name}. \n{self.room}'
+        return f'  Welcome, {self.name}. \n{self.current_room}'
